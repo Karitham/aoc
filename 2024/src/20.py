5743cc507b7080f872a4f1029d3220f61211a9f2
Karitham <kar@karitham.dev>
2024-12-20 14:04:03 +0000

    p2 wip

--- 2024/src/20.py
+++ 2024/src/20.py
@@ -6,7 +6,7 @@ directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 def main():
     grid = [list(lines) for lines in sys.stdin.read().strip().splitlines()]
-    print(f"part_one: {paths(grid, 100, 2)}")
+    print(f"part_one: {paths(grid, 100, 2)}, part_two: {paths(grid, 100, 2)}")
 
 
 def print_grid(g: list[list[str]]):
@@ -28,6 +28,10 @@ def find(grid: list[list[str]], letter: str) -> tuple[int, int]:
     return (pos[0], pos[1])
 
 
+def manhattan_distance(a: tuple[int, int], b: tuple[int, int]) -> int:
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
 def paths(grid: list[list[str]], skip_at_least: int, cheat_blocks: int):
     pathcount = 0
     visited = set()
@@ -35,21 +39,55 @@ def paths(grid: list[list[str]], skip_at_least: int, cheat_blocks: int):
     def in_bounds(x: int, y: int) -> bool:
         return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
 
+    def path_len(start: tuple[int, int], end: tuple[int, int]) -> int:
+        visited = set()
+        queue = [(0, start)]
+
+        while queue:
+            cost, pos = queue.pop(0)
+            if pos == end:
+                return cost
+
+            if pos in visited:
+                continue
+
+            visited.add(pos)
+
+            for dx, dy in directions:
+                new_x, new_y = pos[0] + dx, pos[1] + dy
+                if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
+                    if grid[new_x][new_y] != "#":
+                        queue.append((cost + 1, (new_x, new_y)))
+
+        return None
+
     def find_wall_path(
-        pos: tuple[int, int], wall_pos: tuple[int, int]
+        path_pos: tuple[int, int], wall_pos: tuple[int, int]
     ) -> list[tuple[int, int]]:
         wall_x, wall_y = wall_pos
-        for steps in range(1, cheat_blocks):
-            for dx2, dy2 in directions:
-                next_x, next_y = wall_x + dx2 * steps, wall_y + dy2 * steps
-                if in_bounds(next_x, next_y) and grid[next_x][next_y] != "#":
-                    end_pos = (next_x, next_y)
-                    if end_pos in visited:  # dont backtrack
-                        continue
-                    p = path_len(grid, pos, end_pos)
-                    if (p - steps) > skip_at_least and end_pos != pos:
-                        return end_pos, p - steps
-        return None
+        end_pos_candidates = set()
+        for dx, dy in directions:
+            for i in range(1, cheat_blocks):
+                end_x, end_y = wall_x + dx * i, wall_y + dy * i
+                if (
+                    in_bounds(end_x, end_y)
+                    and grid[end_x][end_y] != "#"
+                    and (end_x, end_y) not in visited
+                    and manhattan_distance(path_pos, (end_x, end_y)) <= cheat_blocks
+                    and (end_x, end_y) != wall_pos
+                ):
+                    end_pos_candidates.add((end_x, end_y))
+        s = 0
+        for end_pos in end_pos_candidates:
+            p = path_len(path_pos, end_pos)
+            # print_grid_with_path(grid, [path_pos, wall_pos, end_pos])
+            if (
+                p
+                and end_pos != path_pos
+                and (p - manhattan_distance(path_pos, end_pos)) >= skip_at_least
+            ):
+                s += 1
+        return s
 
     start = find(grid, "S")
     end = find(grid, "E")
@@ -71,36 +109,11 @@ def paths(grid: list[list[str]], skip_at_least: int, cheat_blocks: int):
 
             if grid[new_x][new_y] == "#":
                 wall_path = find_wall_path(pos, (new_x, new_y))
-                if wall_path and wall_path[0] != pos:
-                    pathcount += 1
+                if wall_path:
+                    pathcount += wall_path
             else:
                 queue.append((cost + 1, new_pos))
     return pathcount
-
-
-def path_len(
-    grid: list[list[str]], start: tuple[int, int], end: tuple[int, int]
-) -> int:
-    visited = set()
-    queue = [(0, start)]
-
-    while queue:
-        cost, pos = queue.pop(0)
-        if pos == end:
-            return cost
-
-        if pos in visited:
-            continue
-
-        visited.add(pos)
-
-        for dx, dy in directions:
-            new_x, new_y = pos[0] + dx, pos[1] + dy
-            if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
-                if grid[new_x][new_y] != "#":
-                    queue.append((cost + 1, (new_x, new_y)))
-
-    return None
 
 
 if __name__ == "__main__":
